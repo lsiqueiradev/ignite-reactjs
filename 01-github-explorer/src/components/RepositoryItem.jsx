938a115faf6ab8a8fb78be067400e13e09677b83
Lucas Siqueira <lucas@lsiqueira.dev>
2022-05-17 04:53:28 -0300

    feat: github explorer | propriedades no react

---
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,15 @@
+function RepositoryItem ({ repository}) {
+  return (
+    <li>
+      <strong>{repository.name ?? 'No name'}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.link}>
+        Acessar repositório
+      </a>
+    </li>
+  );
+}
+
+export { RepositoryItem };
+
