3dd55909cf74347d71ca0efabc716ab09e961886
Lucas Siqueira <lucas@lsiqueira.dev>
2022-05-18 06:12:17 -0300

    feat: dtmoney | configurando miragejs

--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import * as S from './styles';
 
 function TransactionsTable() {
+  useEffect(() => {
+    fetch('https://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  },[])
+
   return (
     <S.Container>
       <table>
