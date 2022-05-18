ca9d5db2fafec1d157f4775fe4f3989f099e3dfa
Lucas Siqueira <lucas@lsiqueira.dev>
2022-05-18 05:52:53 -0300

    feat: dtmoney | componente summary

---
+++ 02-dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from '../Summary';
+import * as S from './styles';
+
+function Dashboard() {
+  return (
+    <S.Container>
+      <Summary />
+    </S.Container>
+  );
+};
+
+export { Dashboard };
