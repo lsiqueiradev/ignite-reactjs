98093067192a6d8ad914773446cff383fdf9a3c4
Lucas Siqueira <lucas@lsiqueira.dev>
2022-06-04 14:38:51 -0300

    feat: Componente: Header

---
+++ 03-ignews/src/ components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import styles from './styles.module.scss';
+
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image width="110" height="31" layout="intrinsic" src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
